8ff1be4b1b1467d1f8ed110eecae53c43aa21606
Ayush_Kumar_Singh <ayushsingh.bodra@gmail.com>
2026-08-07 23:42:08 +0530

    😷  created the logic of the limiters

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Waypoint
+
+URL Shortener at Scale
+
+Features:
+
+Short URL generation
+Analytics dashboard
+Click tracking
+Custom aliases
+QR codes
+
+Advanced:
+
+Redis caching
+Rate limiting
+Docker
+AWS deployment
+
+I'll learn:
+
+Database indexing
+Caching
+Load handling
+API design
--- waypoint/src/main/java/com/ayush/waypoint/config/RateLimitConfig.java
+++ waypoint/src/main/java/com/ayush/waypoint/config/RateLimitConfig.java
@@ -1,12 +1,19 @@
 package com.ayush.waypoint.config;
+
+import com.ayush.waypoint.filter.RateLimitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import com.ayush.waypoint.filter.RateLimitFilter;
-
 @Configuration
 public class RateLimitConfig {
     @Bean
-    public RateLimitFilter rateLimitFilter() {
-        return new RateLimitFilter();
+    public FilterRegistrationBean<RateLimitFilter> rateLimitFilterRegistration(RateLimitFilter rateLimitFilter) {
+        FilterRegistrationBean<RateLimitFilter> registrationBean = new FilterRegistrationBean<>();
+        registrationBean.setFilter(new RateLimitFilter());
+        registrationBean.addUrlPatterns("/api/shorturl/*");
+        registrationBean.setName("rateLimitFilter");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
     }
 }
